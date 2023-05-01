d587507ed022f90c1e1f6a7d37275f5a44310c57
RajatK <78610455+Rajat255@users.noreply.github.com>
2023-05-01 13:17:51 +0000

    corrected naming

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -11,29 +11,29 @@ const Landing = () => {
 
     return (
         <div class="landing-container">
-            <div class="slider">
-                <div class="slides">
-                    <div class="slide">
+            <div class="landing-slider">
+                <div class="landing-slides">
+                    <div class="landing-slide">
                         <img src="/images/back.svg" alt="Image 1" />
                             <h3>Posture Reminders</h3>
                     </div>
-                    <div class="slide">
+                    <div class="landing-slide">
                         <img src="/images/ear.svg" alt="Image 2" />
                             <h3>Ear Care Reminders</h3>
                     </div>
-                    <div class="slide">
+                    <div class="landing-slide">
                         <img src="/images/eye.svg" alt="Image 3" />
                             <h3>Eye Care Reminders</h3>
                     </div>
-                    <div class="slide">
+                    <div class="landing-slide">
                         <img src="/images/stress.svg" alt="Image 4" />
                             <h3>Distracting Videos</h3>
                     </div>
-                    <div class="slide">
+                    <div class="landing-slide">
                         <img src="/images/battery.svg" alt="Image 5" />
                             <h3>Charging Reminders</h3>
                     </div>
-                    <div class="slide">
+                    <div class="landing-slide">
                         <img src="/images/water.svg" alt="Image 6" />
                             <h3>Water Reminders</h3>
                     </div>
